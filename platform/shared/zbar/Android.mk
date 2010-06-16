7b4df64751c5f14a6ddd7e22facea80e31e3fd83
Igor Tarasov <igor@rhomobile.com>
2010-06-16 14:25:46 +0400

    Issue 3712997: adding ZBar barcode reading library sources and rhomobile native extenstions related.

---
+++ platform/shared/zbar/Android.mk
@@ -0,0 +1,30 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := zbarjni
+LOCAL_SRC_FILES := config.c \
+	error.c \
+	symbol.c \
+	convert.c \
+	processor.c \
+	refcnt.c \
+	window.c \
+	video.c \
+	img_scanner.c \
+	scanner.c \
+	image.c \
+	decoder.c \
+	decoder/ean.c \
+	decoder/code128.c \
+	decoder/code39.c \
+	decoder/i25.c \
+	processor/posix.c \
+	processor/lock.c \
+	processor/null.c \
+	video/null.c \
+	window/null.c \
+	zbarjni.c \
+	rhoextension_init.c
+
+include $(BUILD_SHARED_LIBRARY)
